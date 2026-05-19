8c34da4cbe962e0d7a8c65228ad1f22df0296b9a
Manuuhbs <manuela.carlesso@universo.univates.br>
2026-05-19 00:27:58 -0300

    alterações finais

--- views/colar.php
+++ views/colar.php
@@ -5,35 +5,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 $colares = $controller->listar();
 ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-<title>Colar</title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Colar</title>
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-  <form method="post" action="">
-    <label>Código de Barras</label>
-    <input type="text" name="cdbarras" maxlength="13" required>
-    <label>Descrição</label>
-    <input type="text" name="descricao" required>
-    <label>Preço</label>
-    <input type="number" name="preco" step="0.01" required>
-    <button type="submit">Salvar</button>
-    <a href="index.php">Voltar</a>
-  </form>
-  <table>
-    <tr>
-      <th>ID</th>
-      <th>Código de Barras</th>
-      <th>Descrição</th>
-      <th>Preço</th>
-    </tr>
-    <?php foreach ($colares as $colar): ?>
+  <section class="parte-esquerda-colar">
+  </section>
+  <main>
+    <form method="post" action="" class="form">
+      <label>Código de Barras</label>
+      <input type="text" name="cdbarras" maxlength="13" required>
+      <label>Descrição</label>
+      <input type="text" name="descricao" required>
+      <label>Preço</label>
+      <input type="number" name="preco" step="0.01" required>
+      <div class="botoes">
+        <button type="submit">Salvar</button>
+        <button><a href="index.php">Voltar</a></button>
+      </div>
+    </form>
+    <table class="tabela">
       <tr>
-        <td><?= $colar->getIdColar() ?></td>
-        <td><?= $colar->getCdBarras() ?></td>
-        <td><?= $colar->getDescricao() ?></td>
-        <td><?= $colar->getPreco() ?></td>
+        <th>ID</th>
+        <th>Código de Barras</th>
+        <th>Descrição</th>
+        <th>Preço</th>
       </tr>
-    <?php endforeach; ?>
-  </table>
+      <?php foreach ($colares as $colar): ?>
+        <tr>
+          <td><?= $colar->getIdColar() ?></td>
+          <td><?= $colar->getCdBarras() ?></td>
+          <td><?= $colar->getDescricao() ?></td>
+          <td><?= $colar->getPreco() ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
+  </main>
+  <section class="parte-direita-colar">
+  </section>
 </body>
+
+</html>
